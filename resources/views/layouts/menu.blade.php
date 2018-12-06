78dc6664a91852216f43e09256f2409e817da409
luis hoyos <luishoyosolaya@gmail.com>
2018-12-06 17:27:34 -0500

    n

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -11,7 +11,7 @@
         <span class="hide-menu">Contact Types</span>
     </a> 
 </li>
-
+                                                                                                                                                                                                                                                     
 <li> <a href="{{ route('home') }}" class="waves-effect  {{ Request::is('contacts*') ? 'active' : '' }}">
      <i data-icon="7" class="linea-icon linea-basic fa-fw"></i><span class="hide-menu">Contactos<span class="fa arrow">
         </span></span></a>
